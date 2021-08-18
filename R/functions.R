bf50049e995f333e05c57fa0e7c6067b659e6cb8
certara-alargajolli <84907172+certara-alargajolli@users.noreply.github.com>
2021-08-18 11:17:15 +0200

    Update functions.R
    Undo test

--- R/functions.R
+++ R/functions.R
@@ -1,7 +1,6 @@
 #' Setup model info function (@author: certara-alargajolli to check)
 #' 
 #' 
-#test
 
 parframe2setup <- function(run_dir, run_prefix, runno, bootstrap = NULL, read.bootstrap = NULL, boot.obj = NULL, run_dir.boot = NULL, runno.boot = NULL, conf.level = 0.95, min_suc = TRUE, yaml.file = NULL) {  #modified
   
