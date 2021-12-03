ce465029977371b296e6694816e0063066af8eab
certara-alargajolli <84907172+certara-alargajolli@users.noreply.github.com>
2021-12-03 11:56:09 +0100

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -33,7 +33,7 @@ parframe2setup <- function(run_dir, run_prefix, runno, bootstrap = NULL, run_dir
     boot_dir <- paste0(run_dir, run_dir.boot)  
     boot_res <- sprintf("/raw_results_%s%s.csv", run_prefix, ifelse(is.null(runno.boot),runno,runno.boot))
     boot   <- read.csv(paste0(boot_dir, boot_res), header = TRUE, check.names = FALSE)
-  } else if (have.bootstrap & have.boot.obj) {
+  } else if (have.bootstrap & read.bootstrap & have.boot.obj) {
     boot <- boot.obj
   }
   
